459b3fe5eb15c94095c9333955b8348fb13b7d35
naklibeta <admin.it@naklibeta.com>
2021-12-13 10:43:25 +0530

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ServiceProvider',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
